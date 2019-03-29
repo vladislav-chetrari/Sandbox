7ea0b106fa490d8aed83a4d123c2fd68ecc5d8f3
vchetrari <vladislav.chetrari@endava.com>
2019-03-29 16:57:57 +0200

    initial commit

---
+++ app/src/main/java/io/sandbox/unsplash/app/App.kt
@@ -0,0 +1,10 @@
+package io.sandbox.unsplash.app
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/io/sandbox/unsplash/app/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.sandbox.unsplash.app.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.sandbox.unsplash.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
