3c435a0c91e09d26161503fa94fdece2e19676e4
Vladislav Chetrari <vladislav.chetrari@endava.com>
2019-05-08 09:55:17 +0000

    Feature/multitouch

--- app/src/main/java/io/sandbox/unsplash/app/main/multitouch/MultitouchFragment.kt
+++ app/src/main/java/io/sandbox/unsplash/app/main/multitouch/MultitouchFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 
 class MultitouchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        FrameLayout(context!!)
+        MultitouchView(context!!)
 }
