733df0b7534aab4687ce153b11ae719889ce73fb
vchetrari <vladislav.chetrari@endava.com>
2019-05-10 13:17:31 +0300

    MultitouchView moved to widget package

--- app/src/main/java/io/sandbox/app/main/multitouch/MultitouchFragment.kt
+++ app/src/main/java/io/sandbox/app/main/multitouch/MultitouchFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.sandbox.app.widget.MultitouchView
 
 class MultitouchFragment : Fragment() {
 
--- app/src/main/java/io/sandbox/app/widget/MultitouchView.kt
+++ app/src/main/java/io/sandbox/app/widget/MultitouchView.kt
@@ -1,4 +1,4 @@
-package io.sandbox.app.main.multitouch
+package io.sandbox.app.widget
 
 import android.annotation.SuppressLint
 import android.content.Context
