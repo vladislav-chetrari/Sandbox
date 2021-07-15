927bc8f9f76d28bf73dca0c5aa5cc79adf4ba649
Vladislav Chetrari <Vladislav.Chetrari@endava.com>
2021-07-15 13:49:22 +0300

    App: camera config bound

--- app/src/main/java/io/sandbox/app/App.kt
+++ app/src/main/java/io/sandbox/app/App.kt
@@ -1,15 +1,19 @@
 package io.sandbox.app
 
 import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
-class App : Application() {
+class App : Application(), CameraXConfig.Provider {
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(DebugTree())
     }
+
+    override fun getCameraXConfig() = Camera2Config.defaultConfig()
 }
