f615fcbd7df50e3814d10da6830f4315ca45701f
Vladislav Chetrari <Vladislav.Chetrari@endava.com>
2021-07-20 15:51:20 +0300

    MultitouchView: added touch points info

--- app/src/main/java/io/sandbox/app/widget/MultitouchView.kt
+++ app/src/main/java/io/sandbox/app/widget/MultitouchView.kt
@@ -7,19 +7,28 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Point
 import android.util.AttributeSet
+import android.view.KeyEvent.ACTION_UP
 import android.view.MotionEvent
 import android.view.View
+import io.sandbox.R
+import io.sandbox.app.extension.color
 
 class MultitouchView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private val pointers = arrayListOf<Point>()
+    private val pointers = mutableListOf<Point>()
     private val paint = Paint().apply {
         style = Paint.Style.FILL
         color = Color.WHITE
         isAntiAlias = true
         strokeWidth = 1.dp
+    }
+    private val textPaint = Paint().apply {
+        style = Paint.Style.FILL
+        color = context.color(R.color.colorPrimaryLight)
+        isAntiAlias = true
+        textSize = 14.sp
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -38,15 +47,34 @@ class MultitouchView @JvmOverloads constructor(
         super.onDraw(canvas)
         canvas ?: return
         canvas.drawColor(Color.BLACK)
-        pointers.forEach { point ->
-            canvas.run {
-                drawLine(0f, point.y.toFloat(), width.toFloat(), point.y.toFloat(), paint)
-                drawLine(point.x.toFloat(), 0f, point.x.toFloat(), height.toFloat(), paint)
-                drawCircle(point.x.toFloat(), point.y.toFloat(), 24.dp, paint)
-            }
+        pointers.forEachIndexed { index, point ->
+            point.drawTouchTargetOn(canvas)
+            point.drawTouchInfoOn(index, canvas)
         }
     }
 
+    private fun Point.drawTouchTargetOn(canvas: Canvas) {
+        canvas.drawLine(0f, y.toFloat(), width.toFloat(), y.toFloat(), paint)
+        canvas.drawLine(x.toFloat(), 0f, x.toFloat(), height.toFloat(), paint)
+        canvas.drawCircle(x.toFloat(), y.toFloat(), 24.dp, paint)
+    }
+
+    private fun Point.drawTouchInfoOn(
+        pointIndex: Int,
+        canvas: Canvas,
+        paddingStart: Float = 16.dp,
+        paddingTop: Float = 16.dp,
+        textMarginTop: Float = 4.dp
+    ) = canvas.drawText(
+        "Touch[$pointIndex] = (x: $x, y: $y)",
+        paddingStart,
+        paddingTop + textPaint.textSize + (textPaint.textSize + textMarginTop) * pointIndex,
+        textPaint
+    )
+
     private val Int.dp: Float
         get() = times(resources.displayMetrics.density)
+
+    private val Int.sp: Float
+        get() = times(resources.displayMetrics.scaledDensity)
 }
